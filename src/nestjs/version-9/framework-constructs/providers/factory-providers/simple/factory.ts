68c1c9ce2e33de415767c95083e4783d4d8aed1d
nikoladstojiljkovic <42273223+nikoladstojiljkovic@users.noreply.github.com>
2022-09-30 14:56:35 +0200

    Improve simple factory provider

--- src/nestjs/version-9/framework-constructs/providers/factory-providers/simple/factory.ts
+++ src/nestjs/version-9/framework-constructs/providers/factory-providers/simple/factory.ts
@@ -1,8 +1,9 @@
 import {Service1} from "./service-1";
 import {Service2} from "./service-2";
+import {ServiceInterface} from "./service.interface";
 
 export class Factory {
-    produce(serviceType: number): Service1 {
+    produce(serviceType: number): ServiceInterface {
         switch (serviceType) {
         case 1:
             return new Service1();
